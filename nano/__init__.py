04da88c3428191716b86197a3fa488984ce5023a
Rans. <code@rans.page>
2025-10-18 16:00:36 +0300

    chore: refactor bigram lm to 1st iter of gpt

---
+++ nano/__init__.py
@@ -0,0 +1,60 @@
+import torch
+
+from types import SimpleNamespace
+
+torch.manual_seed(1337)
+
+with open('./data/SHAKESPEARE', 'r', encoding='utf-8') as data_file:
+  _text = data_file.read()
+  _vocab = sorted(list(set(_text)))
+data_tool = SimpleNamespace(
+    c2i = { c:i for i, c in enumerate(_vocab) },
+    i2c = { i:c for i, c in enumerate(_vocab) },
+)
+encode = lambda txt: [data_tool.c2i[c] for c in txt]
+decode = lambda vec: ''.join([data_tool.i2c[i] for i in vec])
+_data = torch.tensor(encode(_text), dtype=torch.long)
+_1st_split = int(0.9 * _data.shape[0])
+data_sets = SimpleNamespace(
+    training_data = _data[:_1st_split],
+    validation_data = _data[_1st_split:]
+)
+
+
+def _get_device():
+    for device in [torch.cuda, torch.mps]:
+        if device.is_available():
+            return device.__name__.split(".")[-1]
+    return None
+
+hyper_params = SimpleNamespace(
+    device = _get_device(),
+    batch_size = 32,
+    block_size = 8,
+    eval_iters = 200,
+    eval_interval = 300,
+    learning_rate = 1e-3,
+    training_epochs = 10**4,
+    vocab_size = len(_vocab),
+)
+
+def make_batch(data):
+    last_possible_index = (data.shape[0] - hyper_params.block_size) - 1
+    random_indexes = torch.randint(last_possible_index + 1, (hyper_params.batch_size,))
+    Xs_batch = torch.stack([data[index:index + hyper_params.block_size] for index in random_indexes])
+    Ys_batch = torch.stack([data[index + 1:index + hyper_params.block_size + 1] for index in random_indexes])
+    return Xs_batch.to(hyper_params.device), Ys_batch.to(hyper_params.device)
+
+@torch.no_grad()
+def estimate_loss(model):
+    output = dict()
+    model.eval()
+    for data_name, data in vars(data_sets).items():
+        losses = torch.zeros(hyper_params.eval_iters)
+        for eval_iter in range(hyper_params.eval_iters):
+            Xs_batch, Ys_batch = make_batch(data)
+            logits, loss = model(Xs_batch, Ys_batch)
+            losses[eval_iter] = loss.item()
+        output[data_name] = losses.mean()
+    model.train()
+    return output
---
+++ nano/__main__.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+
+from torch.nn import functional as F
+
+from . import hyper_params, data_sets, data_tool
+from . import decode, make_batch, estimate_loss
+
+class BigramLM(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self._embeddings = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, X, Y=None):
+        logits = self._embeddings(X)
+        if Y is None:
+            return logits, None
+        _B, _T, C = logits.shape
+        logits = logits.view(-1, C)
+        target = torch.flatten(Y)
+        loss = F.cross_entropy(logits, target)
+        return logits, loss
+
+    def generate(self, x, tokens):
+        for _ in range(tokens):
+            logits, loss = self(x)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            y_hat = torch.multinomial(probs, num_samples=1)
+            x = torch.cat((x, y_hat), dim=1)
+        return x
+
+def train(model):
+    optimizer = torch.optim.AdamW(model.parameters(), lr=hyper_params.learning_rate)
+    for epoch in range(hyper_params.training_epochs):
+        if epoch % hyper_params.eval_interval == 0:
+            losses = estimate_loss(model)
+            step = epoch + 1
+            training_loss = losses["training_data"]
+            validation_loss = losses["validation_data"]
+            print(f"{step=:4d} {training_loss=:.4f}, {validation_loss=:.4f}")
+        Xs_batch, Ys_batch = make_batch(data_sets.training_data)
+        logits, loss = model(Xs_batch, Ys_batch)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+
+def main():
+    model = BigramLM(hyper_params.vocab_size)
+    model = model.to(hyper_params.device)
+    train(model)
+    context = torch.zeros((1, 1), dtype=torch.long, device=hyper_params.device)
+    print(decode(model.generate(context, tokens=500)[0].tolist()))
+
+if __name__ == "__main__":
+    main()
